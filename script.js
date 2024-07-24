e791d710254b68e5e8563e3886a6c7eda21a8152
Divisha Surana <24951558-DivishaSurana@users.noreply.replit.com>
2024-07-24 17:25:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+var button = $('.views');
+var header = $('.two');
+button.on('click', doSomething);
+
+ function doSomething() {
+   header.removeClass('hidden')
+ }
+
+var buttonsix = $('.culture')
+var headersix = $('.two');
+buttonsix.on('click', doSomethingsix);
+
+ function doSomethingsix() {
+   headersix.removeClass('hidden')
+ }
+
+var buttonseven = $('.history')
+var headerseven = $('.seven');
+buttonseven.on('click', doSomethingseven);
+
+ function doSomethingseven() {
+   headerseven.removeClass('hidden')
+ }
+
+var buttontwo = $('.water')
+var headertwo = $('.three');
+buttontwo.on('click', doSomethingtwo);
+
+ function doSomethingtwo() {
+   headertwo.removeClass('hidden')
+ }
+
+var buttonthree = $('.warm')
+var headerthree = $('.four');
+buttonthree.on('click', doSomethingthree);
+
+ function doSomethingthree() {
+   headerthree.removeClass('hidden')
+ }
+
+var buttonfour = $('.cool')
+var headerfour = $('.five');
+buttonfour.on('click', doSomethingfour);
+
+ function doSomethingfour() {
+   headerfour.removeClass('hidden')
+ }
+
+var buttoneight = $('.no')
+var headereight = $('.eight');
+buttoneight.on('click', doSomethingeight);
+
+ function doSomethingeight() {
+   headereight.removeClass('hidden')
+ }
+
+var buttonnine = $('.yes')
+var headernine = $('.nine');
+buttonnine.on('click', doSomethingnine);
+
+ function doSomethingnine() {
+   headernine.removeClass('hidden')
+ }
+
+var buttonten = $('.yestwo')
+var headerten = $('.ten');
+buttonten.on('click', doSomethingten);
+
+ function doSomethingten() {
+   headerten.removeClass('hidden')
+ }
+
+var buttoneleven = $('.notwo')
+var headereleven = $('.eleven');
+buttoneleven.on('click', doSomethingeleven);
+
+ function doSomethingeleven() {
+   headereleven.removeClass('hidden')
+ }
